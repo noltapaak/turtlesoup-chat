3651d821aaaf58d18ce5d6cbf96e1039d04e0a42
박성희 <bagseonghui@bagseonghuiui-MacBookPro.local>
2025-06-04 00:18:28 +0900

    fix: 이미지 플레이스홀더를 Unsplash로 변경 및 설정 업데이트

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
         port: '', 
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
